559130648a837588c99fedfd9dce8c1f1faa3812
Jason Lenoble <jason.lenoble@gmail.com>
2016-12-09 19:28:22 +0100

    Redefined GulpGlob as a Polyton
    Currently singleton test fail for returned value of dest. Some
    preprocessing is required before implementing the top level dest method.

--- src/gulpglob.js
+++ src/gulpglob.js
@@ -1,86 +1,17 @@
 import gulp from 'gulp';
 import path from 'path';
 import isValidGlob from 'is-valid-glob';
-import {SingletonFactory} from 'singletons';
+import SimpleGulpGlob from './simple-gulpglob';
+import {PolytonFactory} from 'polyton';
 
-const _ready = Symbol();
-
-class GulpGlob {
-
-  constructor(glob, options = {ready: Promise.resolve()}) {
-    if (!isValidGlob(glob)) {
-      throw new TypeError('Invalid glob element: "' + glob + '"');
+const GulpGlob = PolytonFactory(SimpleGulpGlob, ['literal'], undefined, {
+  extend: {
+    toPromise() {
+      return Promise.all(this.map(el => el.toPromise()));
+    },
+    dest(dir) {
     }
-
-    if (!Array.isArray(glob)) {glob = [glob];}
-
-    const _base = process.cwd();
-    const _glob = glob.map(glb => path.relative(_base, glb));
-
-    this[_ready] = options.ready;
-
-    Object.defineProperties(this, {
-      glob: {
-        get() {return _glob;}
-      },
-      base: {
-        get() {return _base;}
-      }
-    });
   }
+});
 
-  isReady() {
-    return this[_ready];
-  }
-
-  src() {
-    return gulp.src(this.glob, {base: this.base});
-  }
-
-  toPromise() {
-    return this.isReady().then(() => this);
-  }
-
-  list() {
-    return this.isReady().then(() => new Promise((resolve, reject) => {
-      var list = [];
-      this.src()
-        .on('data', file => {
-          list.push(file.history[0]);
-        })
-        .on('end', () => {
-          console.log(list.join('\n'));
-          resolve(list);
-        });
-    }));
-  }
-
-  dest(dest) {
-    return new GulpGlobSingleton(this.glob.map(glb => {
-      var a = glb.split('**');
-      a[0] = path.join(dest, a[0]);
-
-      if (a.length === 1) {
-        return a[0];
-      } else {
-        return path.join(a[0], '**', a[1]);
-      }
-    }), {
-      ready: new Promise((resolve, reject) => {
-        this.src().pipe(gulp.dest(dest))
-          .on('error', reject)
-          .on('end', resolve);
-      })
-    });
-  }
-
-}
-
-const GulpGlobSingleton = SingletonFactory(GulpGlob, [
-  'literal',
-  {
-    property: 'ready'
-  }
-]);
-
-export default GulpGlobSingleton;
+export default GulpGlob;
