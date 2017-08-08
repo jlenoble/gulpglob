c3b399cca34494094bac72eb2240e3304dbd5bd7
Jason Lenoble <jason.lenoble@gmail.com>
2017-08-08 09:38:26 +0200

    Added 'src/*.js' tests and reorganized

--- test/various-paths.test.js
+++ test/various-paths.test.js
@@ -22,26 +22,76 @@ describe(`Testing with various args`, function () {
       path.join(cwd, 'src/simple-gulpglob.js')], {cwd: 'src'}],
     [[path.join(cwd, 'src/**/*.js')], {cwd: path.join(cwd, 'src')}],
     [[path.join(cwd, 'src/gulpglob.js'),
-      path.join(cwd, 'src/simple-gulpglob.js')], {cwd: path.join(cwd, 'src')}],
+      path.join(cwd, 'src/simple-gulpglob.js')], {
+      cwd: path.join(cwd, 'src'),
+    }],
   ];
 
-  srcArgs.forEach(args => {
-    it(`Comparing SimpleGulpGlob with gulp.src(${JSON.stringify(args[0])
-    }${args[1] ? `, ${JSON.stringify(args[1])}` : ''})`, function () {
-      const ggSrc = new SimpleGulpGlob(...args);
+  const srcArgs2 = [
+    ['src/*.js'],
+    [['src/gulpglob.js', 'src/simple-gulpglob.js']],
+    [['*.js'], {cwd: 'src'}],
+    [['gulpglob.js', 'simple-gulpglob.js'], {cwd: 'src'}],
+    [path.join(cwd, 'src/*.js')],
+    [[path.join(cwd, 'src/gulpglob.js'),
+      path.join(cwd, 'src/simple-gulpglob.js')]],
+    [['*.js'], {cwd: path.join(cwd, 'src')}],
+    [['gulpglob.js', 'simple-gulpglob.js'], {cwd: path.join(cwd, 'src')}],
+    [[path.join(cwd, 'src/*.js')], {cwd: 'src'}],
+    [[path.join(cwd, 'src/gulpglob.js'),
+      path.join(cwd, 'src/simple-gulpglob.js')], {cwd: 'src'}],
+    [[path.join(cwd, 'src/*.js')], {cwd: path.join(cwd, 'src')}],
+    [[path.join(cwd, 'src/gulpglob.js'),
+      path.join(cwd, 'src/simple-gulpglob.js')], {
+      cwd: path.join(cwd, 'src'),
+    }],
+  ].reverse();
 
-      return Promise.all([
-        equalStreamContents(ggSrc.src(), gulp.src(...args)),
-        equalStreamContents(ggSrc.src(), gulp.src('src/**/*.js')),
-      ]);
+  describe(`Pattern 'src/**/*.js'`, function () {
+    srcArgs.forEach(args => {
+      it(`Comparing SimpleGulpGlob with gulp.src(${JSON.stringify(args[0])
+      }${args[1] ? `, ${JSON.stringify(args[1])}` : ''})`, function () {
+        const ggSrc = new SimpleGulpGlob(...args);
+
+        return Promise.all([
+          equalStreamContents(ggSrc.src(), gulp.src(...args)),
+          equalStreamContents(ggSrc.src(), gulp.src('src/**/*.js')),
+        ]);
+      });
+    });
+
+    it('GulpGlob set with equivalent globs is properly reduced', function () {
+      const gg1 = new GulpGlob(srcArgs[0]);
+      const gg2 = new GulpGlob(...srcArgs); // All 12 globs yield the same and
+      // GulpGlob knows it
+
+      expect(gg1).to.equal(gg2);
+      expect(gg2.glob).to.eql(['src/**/*.js']);
+      expect(gg2.cwd).to.eql(cwd);
     });
   });
 
-  it('GulpGlob set with equivalent globs is properly reduced', function () {
-    const gg1 = new GulpGlob(srcArgs[0]);
-    const gg2 = new GulpGlob(...srcArgs); // All 12 globs yield the same and
-    // GulpGlob knows it
+  describe(`Pattern 'src/*.js'`, function () {
+    srcArgs2.forEach(args => {
+      it(`Comparing SimpleGulpGlob with gulp.src(${JSON.stringify(args[0])
+      }${args[1] ? `, ${JSON.stringify(args[1])}` : ''})`, function () {
+        const ggSrc = new SimpleGulpGlob(...args);
 
-    expect(gg1).to.equal(gg2);
+        return Promise.all([
+          equalStreamContents(ggSrc.src(), gulp.src(...args)),
+          equalStreamContents(ggSrc.src(), gulp.src('src/**/*.js')),
+        ]);
+      });
+    });
+
+    it('GulpGlob set with equivalent globs is properly reduced', function () {
+      const gg1 = new GulpGlob(srcArgs2[1]);
+      const gg2 = new GulpGlob(...srcArgs2); // All 12 globs yield the same and
+      // GulpGlob knows it
+
+      expect(gg1).to.equal(gg2);
+      expect(gg2.glob).to.eql(['*.js']);
+      expect(gg2.cwd).to.eql(path.join(cwd, 'src'));
+    });
   });
 });
